afcc7b78b74a87f81e529fd7a1876094a62d59cd
Amal C P <acp070146@gmail.com>
2024-04-15 23:28:21 +0530

    some

--- src/pages/story/components/team/index.tsx
+++ src/pages/story/components/team/index.tsx
@@ -1,3 +1,4 @@
+import { LinkedInIcon } from "./assets/svg";
 import styles from "./index.module.css";
 
 
@@ -7,5 +8,14 @@ export const Team = () => {
 
 
 export const TeamCard = () => {
-  return <div className={styles.Wrapper}>Team</div>;
+  return <div className={styles.Wrapper}>
+    <img src="" alt="" />
+    <div>
+      <div>
+        <h2>Name Surname</h2>
+        <p>Designation</p>
+      </div>
+      <a href=""><LinkedInIcon /></a>
+    </div>
+  </div>;
 };
