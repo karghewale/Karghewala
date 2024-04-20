f9c01209eb38cdc65b70ae1e06db53fb9d226081
Amal C P <acp070146@gmail.com>
2024-04-20 10:24:31 +0530

    team completed

--- src/pages/story/components/team/index.tsx
+++ src/pages/story/components/team/index.tsx
@@ -1,21 +1,65 @@
 import { LinkedInIcon } from "./assets/svg";
+import { DataTeam } from "./data";
 import styles from "./index.module.css";
 
-
 export const Team = () => {
-  return <div className={styles.Wrapper}>Team</div>;
+  return (
+    <div className={styles.Wrapper}>
+      <div>
+        <h2>OUR TEAM</h2>
+        <div className={styles.Grid}>
+          {DataTeam.TEAM.map((member) => (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              designation={member.designation}
+              linkedin={member.linkedin}
+              image={member.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2>OUR ADVISORS</h2>
+        <div className={styles.Grid}>
+          {DataTeam.ADVISORS.map((advisor) => (
+            <TeamCard
+              key={advisor.id}
+              name={advisor.name}
+              designation={advisor.designation}
+              linkedin={advisor.linkedin}
+              image={advisor.image}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-
-export const TeamCard = () => {
-  return <div className={styles.Wrapper}>
-    <img src="" alt="" />
-    <div>
-      <div>
-        <h2>Name Surname</h2>
-        <p>Designation</p>
+interface TeamCardProps {
+  name: string;
+  designation: string;
+  linkedin: string;
+  image: string;
+}
+const TeamCard = ({ name, designation, linkedin, image }: TeamCardProps) => {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={name} className={styles.image} />
+      <div className={styles.details}>
+        <div>
+          {" "}
+          <h3>{name}</h3>
+          <p>{designation}</p>
+        </div>
+        <a
+          href={`https://www.linkedin.com/in/${linkedin}`}
+          className={styles.linkedinLink}
+        >
+          <LinkedInIcon />
+        </a>
       </div>
-      <a href=""><LinkedInIcon /></a>
     </div>
-  </div>;
+  );
 };
