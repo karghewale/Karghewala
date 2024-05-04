1de5b2ad773af1938a0eac04d7673fe6a0ed1c31
Amal C P <acp070146@gmail.com>
2024-05-04 13:21:48 +0530

    updated with alumini

--- src/pages/alumini/components/aluminiModal.tsx
+++ src/pages/alumini/components/aluminiModal.tsx
@@ -1,37 +1,51 @@
+import Marquee from "react-fast-marquee";
 import Modal from "../../../components/modal";
-
+import styles from "../index.module.css";
 type Props = {
   isOpen: boolean;
   onClose: () => void;
   data: AluminiResponseType;
 };
 
-const AluminiModal = ({isOpen, onClose, data}: Props) => {
+const AluminiModal = ({ isOpen, onClose, data }: Props) => {
+  const marqParams = {
+    autoFill: true,
+    pauseOnHover: true,
+    gradient: false,
+    speed: 30,
+    drag: true,
+  };
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-    >
-      <div>
-        <div>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className={styles.individual}>
+        <div className={styles.content}>
           <img src={data?.image} alt="Profile Photo" />
           <div>
             <div>
-              <h2>{data?.name}</h2>
-              <p>{data?.age}</p>
-              <p>₹{data?.salary}/month</p>
-              <p>Avg. sale rate ₹{data?.sale_rate}/month</p>
+              <h2>
+                {data?.name},<p>{data?.age}</p>
+              </h2>
+              {"["}
+              <p>
+                <p>₹{data?.salary}/month</p>
+                <p>Avg. sale rate ₹{data?.sale_rate}/month</p>
+              </p>
+              {"]"}
             </div>
             <p>{data?.description}</p>
           </div>
         </div>
-        {data?.photos.map((item, index) => (
-          <img
-            key={index}
-            src={item}
-            alt="Photos of the Alumini"
-          />
-        ))}
+        <div style={{width:"100%",overflow:"hidden"}}>
+          <Marquee
+            {...marqParams}
+            style={{ width: "100vw" }}
+            className={styles.imgwrapper}
+          >
+            {data?.photos.map((item, index) => (
+              <img key={index} src={item} alt="Photos of the Alumini" />
+            ))}
+          </Marquee>
+        </div>
       </div>
     </Modal>
   );
