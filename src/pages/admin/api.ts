03cc223986dd62a08084a9bf76743643bee8b0a2
Jenin <jenin8282@gmail.com>
2024-05-16 20:32:04 +0530

    chore: slight refactor

--- src/pages/admin/api.ts
+++ src/pages/admin/api.ts
@@ -19,55 +19,6 @@ const seconds = currentDateAndTime.getSeconds();
 
 const formattedDateAndTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
-export const insertBlogs = async (formdata: any) => {
-  const adjustedData = {
-    title: formdata.title,
-    created_at: formattedDateAndTime,
-    author: formdata.author,
-    dateofblog: formdata.dateofblog,
-    extra_images: formdata.extra_images,
-    image: formdata.image,
-    description: formdata.description,
-    category: formdata.categories,
-  };
-  console.log(adjustedData);
-  const { data: blogs, error } = await supabase
-    .from("blogs")
-    .insert([adjustedData])
-    .select();
-
-  if (error) {
-    // Handle the error
-    throw error;
-  } else {
-    return blogs;
-  }
-};
-export const updateBlogs = async (formdata: any) => {
-  const adjustedData = {
-    title: formdata.title,
-    created_at: formattedDateAndTime,
-    author: formdata.author,
-    dateofblog: formdata.dateofblog,
-    extra_images: formdata.extra_images,
-    image: formdata.image,
-    description: formdata.description,
-    category: formdata.categories,
-  };
-  console.log(adjustedData);
-  const { data: blogs, error } = await supabase
-    .from("blogs")
-    .update([adjustedData])
-    .eq("id", formdata.id);
-
-  if (error) {
-    // Handle the error
-    throw error;
-  } else {
-    return blogs;
-  }
-};
-
 // gllery
 export const getGallery = async () => {
   let { data: gallery, error } = await supabase.from("gallery").select("*");
