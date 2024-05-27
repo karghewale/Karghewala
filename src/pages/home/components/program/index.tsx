3820e59a6ac50f1b01568426cb250ebf51335de8
Jenin <jenin8282@gmail.com>
2024-05-27 15:18:25 +0530

    feat: program data done

--- src/pages/home/components/program/index.tsx
+++ src/pages/home/components/program/index.tsx
@@ -1,25 +1,16 @@
-import styles from './index.module.css'
-import image from './image.png'
+import styles from "./index.module.css";
+import image from "./image.png";
 import component from "/assets/sidePlacedComponent.png";
+import data from "../../../../data/home.json";
 
-type Props = {}
-
-export const Program = (_props: Props) => {
+export const Program = () => {
   return (
     <div className={styles.Wrapper}>
-      <h2>PROGRAM INTRO</h2>
+      <h2>{data.program.title}</h2>
       <img className={styles.inhouse} src={image} alt="" />
-      <p>
-        A six-month free training program focusing on developing their skills as
-        creative entrepreneurs followed by a 3-year incubation period. Our goal
-        is to assist artisans by establishing market connections and giving them
-        rightful credit for their unique designs, helping them establish their
-        own distinct design and brand identity. Design and Management programs
-        can be chosen ndependently based on the requirements of the artisan
-        trainees.
-      </p>
+      <p>{data.program.description}</p>
       <img className={styles.component} src={component} alt="" />
       <img className={styles.component2} src={component} alt="" />
     </div>
   );
-}
+};
